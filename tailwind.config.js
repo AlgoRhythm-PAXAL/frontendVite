08402dbd718b893d8f81301f3e56c376881de1fe
ThanukaPerera <thanukalap@gmail.com>
2025-03-23 22:16:34 +0530

    Developing charts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./src/**/*.{html,js}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
 
 /** @type {import('tailwindcss').Config} */
 export default {
