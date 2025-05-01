eaf90daafcc026a9642f64c72dcbdd4fc3e49d75
Kalani Chandrasena <kalanichandrasena99@gmail.com>
2025-05-01 12:09:37 +0530

    Updated inquiry pages

--- src/pages/staff/NewInquiries.jsx
+++ src/pages/staff/NewInquiries.jsx
@@ -38,7 +38,6 @@ const NewInquiries = () => {
         }),
     },
     { label: "Tracking Number", key: "parcelTrackingNo" },
-    
   ];
 
   const actions = [
@@ -53,27 +52,29 @@ const NewInquiries = () => {
 
   return (
     <div className="px-8 py-6">
-    <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center mb-8 gap-4">
-    <div className="flex-1">
-      <h2 className="text-2xl font-semibold text-gray-800 mb-2">Inquiry Management</h2>
-      <p className="text-gray-500">Manage all parcel inquiries</p>
+      <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center mb-8 gap-4">
+        <div className="flex-1">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+            Inquiry Management
+          </h2>
+          <p className="text-gray-500">Manage all parcel inquiries</p>
+        </div>
+
+        <div className="flex gap-4 w-full lg:w-auto">
+          <StatsBox title="Inquiries Today" value="50" />
+          <StatsBox title="Pending Inquiries" value="20" />
+        </div>
+      </div>
+      <div>
+        <DataTable
+          data={inquiries}
+          columns={columns}
+          actions={actions}
+          rowsPerPage={6}
+          textMessage={"No new inquiries"}
+        />
+      </div>
     </div>
-    
-    <div className="flex gap-4 w-full lg:w-auto">
-      <StatsBox title="Inquiries Today" value="50" />
-      <StatsBox title="Pending Inquiries" value="20" />
-    </div>
-  </div>
-   <div >
-     <DataTable
-      data={inquiries}
-      columns={columns}
-      actions={actions}
-      rowsPerPage={6}
-      textMessage={"No new inquiries"}
-    />
-    </div>
-   </div>
   );
 };
 
--- src/pages/staff/ViewRepliedInquiries.jsx
+++ src/pages/staff/ViewRepliedInquiries.jsx
@@ -16,7 +16,7 @@ const ViewRepliedInquiries = () => {
 
       setInquiries(response.data);
     } catch (err) {
-      console.error(err);
+      console.error("Error in fetching replied inquiries", err);
     }
   };
 
@@ -37,7 +37,6 @@ const ViewRepliedInquiries = () => {
         }),
     },
     { label: "Tracking Number", key: "parcelTrackingNo" },
-    
   ];
 
   const actions = [
@@ -46,33 +45,32 @@ const ViewRepliedInquiries = () => {
       className:
         "bg-white text-Primary border-2 border-Primary font-semibold px-5 py-2 rounded-lg hover:shadow-md transition-all duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-Primary-light",
       onClick: (row) =>
-        navigate(`/staff/inquiry-management/view-replied-inquiries/${row.inquiryId}`),
+        navigate(
+          `/staff/inquiry-management/view-replied-inquiries/${row.inquiryId}`
+        ),
     },
   ];
 
   return (
     <div className="px-8 py-6">
-    <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center mb-8 gap-4">
-      <div className="flex-1">
-        <h2 className="text-2xl font-semibold text-gray-800 mb-2">
-          Parcel Inquiry History
-        </h2>
-        <p className="text-gray-500">
-          View past inquiries made by customers
-        </p>
+      <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center mb-8 gap-4">
+        <div className="flex-1">
+          <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+            Parcel Inquiry History
+          </h2>
+          <p className="text-gray-500">View past inquiries made by customers</p>
+        </div>
       </div>
-
+      <div>
+        <DataTable
+          data={inquiries}
+          columns={columns}
+          actions={actions}
+          rowsPerPage={6}
+          textMessage={"No past inquiries "}
+        />
+      </div>
     </div>
-   <div>
-     <DataTable
-      data={inquiries}
-      columns={columns}
-      actions={actions}
-      rowsPerPage={6}
-      textMessage={"No past inquiries "}
-    />
-    </div>
-   </div>
   );
 };
 
